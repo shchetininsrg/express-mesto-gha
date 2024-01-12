f9255a56f7b39a1839abe4e0340a92f1c443f7ce
Sergey Shchetinin <shchetinin.srg@yandex.ru>
2024-01-12 21:37:50 +0500

    first itteration

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
--- models/user.js
+++ models/user.js
@@ -4,14 +4,14 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Минимальная длина имени - 2'],
+    maxlength: [30, 'Максимальная длина имени - 30'],
   },
   about: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Минимальная длина описания - 2'],
+    maxlength: [30, 'Максимальная длина описания - 30'],
   },
   avatar: {
     type: String,
