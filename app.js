223315db629597b7da448afb6b6d21a1ceae510b
Sergey Shchetinin <shchetinin.srg@yandex.ru>
2024-01-13 09:52:03 +0500

    tes)

--- app.js
+++ app.js
@@ -22,4 +22,8 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 app.listen(PORT);
