672bda340486d878bcfcb012bf094ba061deaa11
Sergey Shchetinin <shchetinin.srg@yandex.ru>
2024-01-21 13:48:06 +0500

    14pr 2 itt test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,26 @@
 const jwt = require('jsonwebtoken');
-const { UnauthorizedError } = require('../errors/UnauthorizedError');
+// const { UNAUTHORIZED } = require('../utils/constantErrors');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+
   if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new UnauthorizedError('Необходима авторизация'));
     return;
   }
+
   const token = authorization.replace('Bearer ', '');
   let payload;
+
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
     return;
   }
+
   req.user = payload;
+
   next();
 };
